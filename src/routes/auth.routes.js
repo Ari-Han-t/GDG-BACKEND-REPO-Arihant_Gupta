d289e501ba46a899a0eb960d73cc479908299b26
Arihant <arihantg23042006@gmail.com>
2026-01-14 23:54:43 +0530

    connect auth logic with prisma database

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { register, login } = require("../controllers/auth.controller");
 
-const users = []; // TEMP memory storage
-
-router.post("/register", async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({ message: "Missing fields" });
-  }
-
-  users.push({ email, password });
-  res.status(201).json({ message: "User registered (temporary)" });
-});
-
-router.post("/login", (req, res) => {
-  res.json({ message: "Login endpoint (next step)" });
-});
+router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
